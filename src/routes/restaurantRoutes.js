7f35b2f43449a951b851e78f0643f56b91b54394
Nguyen <nguyenpham9758@gmail.com>
2025-06-13 10:45:43 +0700

    Get detail information of restaurant by slug

--- src/routes/restaurantRoutes.js
+++ src/routes/restaurantRoutes.js
@@ -1,13 +1,9 @@
 import express from 'express';
-import { protect } from '../middleware/authMiddleware.js';
-import { getRestaurantBySlug, getAllRestaurants } from '../controllers/restaurantController.js';
+import { getAllRestaurants } from '../controllers/restaurantController.js';
 
 const router = express.Router();
 
 // Public routes (no authentication required)
-router.get('/:slug', getRestaurantBySlug); // Lấy chi tiết nhà hàng theo slug
-
-// Authenticated routes (require admin login)
-router.get('/', protect, getAllRestaurants); // Lấy danh sách tất cả nhà hàng của admin
+router.get('/', getAllRestaurants); // Lấy danh sách tất cả nhà hàng hoặc chi tiết theo slug
 
 export default router;
